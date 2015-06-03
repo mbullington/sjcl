d394654a3de1101e381112c3423967aa33dc7208
Michael Bullington <mikebullingtn@gmail.com>
2015-06-02 23:31:11 -0400

    This, something, Make artifact.

--- config.mk
+++ config.mk
@@ -1,2 +1,2 @@
-SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/codecBytes.js core/sha256.js core/random.js core/bn.js core/ecc.js 
+SOURCES= core/sjcl.js core/aes.js core/bitArray.js core/codecString.js core/sha256.js core/random.js core/bn.js core/ecc.js core/codecArrayBuffer.js 
 COMPRESS= core_yui.js
